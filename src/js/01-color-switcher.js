b5ae2caed0f36d23a4099b73cca158a989be2724
Daria Tkachenko <113775461+dariaaferreira@users.noreply.github.com>
2023-03-14 02:38:00 +0200

    done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+let timerId = null;
+
+function changeColor () {
+    document.body.style.backgroundColor = getRandomHexColor();
+};
+
+btnStart.addEventListener('click', () => {
+    if (btnStart.disabled = true) {
+        btnStop.disabled = false; 
+        timerId = setInterval(() => {
+            changeColor()
+        }, 1000);
+    };
+});
+
+btnStop.addEventListener("click", () => {
+    if (btnStop.disabled = true) {
+        btnStart.disabled = false;
+        clearInterval(timerId);
+    }
+});
+
